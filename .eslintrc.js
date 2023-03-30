3b5c68a9d79826f0e34895294e3c2459c8f8e5a0
Lam <daibanglam@gmail.com>
2023-03-30 15:21:44 +0700

    feat: turn on necessary rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,10 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    "plugin:import/recommended"
+    'plugin:prettier/recommended'
   ],
   root: true,
   env: {
@@ -19,5 +19,19 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'quotes': [2, 'single', { 'avoidEscape': true }],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': ['PascalCase'],
+        'prefix': ['I']
+      }
+    ],
   },
 };
